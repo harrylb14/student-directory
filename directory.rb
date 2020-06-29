2699d9333e6cd38cebe6cf3029f130f461369a59
harrylb14 <harry.lingardbright@gmail.com>
2020-06-29 14:45:51 +0000

    added the ability to load a file from the command line

--- directory.rb
+++ directory.rb
@@ -4,18 +4,18 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   
   while !name.empty? do
     puts "Please enter the cohort"
-    cohort = gets.chomp.downcase.capitalize
+    cohort = STDIN.gets.chomp.downcase.capitalize
     while !@possible_cohorts.include? cohort
       puts "That is not a recognized cohort, please try again"
-      cohort = gets.chomp.downcase.capitalize
+      cohort = STDIN.gets.chomp.downcase.capitalize
     end
     @students << {name: name, cohort: cohort}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
@@ -76,8 +76,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -85,11 +85,24 @@ def load_students
   file.close
 end
 
-def interactive_menu
-  loop do 
-    print_menu
-    process(gets.chomp)
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist"
+    exit
   end
 end
 
+def interactive_menu
+  loop do 
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+try_load_students
 interactive_menu
