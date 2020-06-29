62f3317bfa8d32eb171e1746f8ff75be4a581b69
harrylb14 <harry.lingardbright@gmail.com>
2020-06-29 15:49:39 +0000

    refactored to use CSV library

--- directory.rb
+++ directory.rb
@@ -1,38 +1,12 @@
+require 'csv'
 @students = []
 @possible_cohorts = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  
-  while !name.empty? do
-    puts "Please enter the cohort"
-    cohort = STDIN.gets.chomp.downcase.capitalize
-    while !@possible_cohorts.include? cohort
-      puts "That is not a recognized cohort, please try again"
-      cohort = STDIN.gets.chomp.downcase.capitalize
-    end
-    @students << {name: name, cohort: cohort}
-    puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
+def interactive_menu
+  loop do 
+    print_menu
+    process(STDIN.gets.chomp)
   end
-  @students
-end
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_student_list
-  @students.each.with_index(1) do |student, number|
-    puts "#{number}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
 end
 
 def print_menu
@@ -42,12 +16,6 @@ def print_menu
   puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end 
 
 def process(selection)
   case selection 
@@ -66,23 +34,66 @@ def process(selection)
   end
 end
 
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  
+  while !name.empty? do
+    puts "Please enter the cohort"
+    cohort = STDIN.gets.chomp.downcase.capitalize
+    while !@possible_cohorts.include? cohort
+      puts "That is not a recognized cohort, please try again"
+      cohort = STDIN.gets.chomp.downcase.capitalize
+    end
+    update_students(name, cohort)
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
   end
-  file.close
+  @students
+end
+
+def update_students(name, cohort)
+  if !cohort.is_a? Symbol
+    cohort = cohort.to_sym
+  end
+  @students << {name: name, cohort: cohort}
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end 
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each.with_index(1) do |student, number|
+    puts "#{number}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  CSV.open("students.csv", "w") do |csv|
+    @students.each do |student|
+      csv << [student[:name], student[:cohort]]
+    end
+  end
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+  CSV.foreach(filename, "r") do |line|
+      name, cohort = line[0], line[1]
+      update_students(name, cohort)
   end
-  file.close
 end
 
 def try_load_students
@@ -94,13 +105,6 @@ def try_load_students
   else 
     puts "Sorry, #{filename} doesn't exist"
     exit
-  end
-end
-
-def interactive_menu
-  loop do 
-    print_menu
-    process(STDIN.gets.chomp)
   end
 end
 
